c4cf9ec06c487349c6f03ea00da5b39556d53049
Dimitris Platis <dimitris.platis@neat.no>
2023-11-21 12:10:23 +0100

    Crash on the native side

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -1,10 +1,12 @@
 #include <jni.h>
 #include <string>
+#include <cassert>
 
 extern "C" JNIEXPORT jstring JNICALL
 Java_com_example_tombstone_MainActivity_stringFromJNI(
         JNIEnv* env,
         jobject /* this */) {
+    assert(false); // You wanted a tombstone, here you go!
     std::string hello = "Hello from C++";
     return env->NewStringUTF(hello.c_str());
 }

6d26c88455dc098ba7612a47c3dc081588e8f765
Dimitris Platis <dimitris.platis@neat.no>
2023-11-21 13:12:58 +0100

    Add a small tracelog for sanity

--- app/src/main/java/com/example/tombstone/MainActivity.java
+++ app/src/main/java/com/example/tombstone/MainActivity.java
@@ -2,10 +2,12 @@ package com.example.tombstone;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
 import com.example.tombstone.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "TombstoneApp";
 
 private ActivityMainBinding binding;
 
@@ -13,8 +15,10 @@ private ActivityMainBinding binding;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-     binding = ActivityMainBinding.inflate(getLayoutInflater());
-     setContentView(binding.getRoot());
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        Log.e(TAG, "This app will crash and generate a tombstone in /data/tombstones/");
 
         // Example of a call to a native method
         TextView tv = binding.sampleText;
